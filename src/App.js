2155f7a26adc613529d97a5216d17acb86cd0440
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-07 14:37:52 -0300

    Upload files

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { Button, Icon } from '@rneui/base'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import UserList from './views/UserForm'
+import UserForm from './views/UserForm'
+// import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const Stack = createNativeStackNavigator()
+
+export default props => {
+  return (
+    <SafeAreaView style={{ flexGrow: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="UserList"
+          screenOptions={{
+            headerStyle: { backgroundColor: '#f4511e' },
+            headerTintColor: '#fff',
+            headerTitleStyle: 'bold',
+            headerShown: true,
+          }}>
+          <Stack.Screen
+            name="UserList"
+            component={UserList}
+            options={({ navigation }) => {
+              return {
+                title: 'Lista de Usuários',
+                headerRight: () => (
+                  <Button
+                    type="clear"
+                    onPress={() => navigation.navigate('UserForm')}
+                    icon={
+                      <Icon
+                        name="add"
+                        size={25}
+                        color="white"
+                      />
+                    }
+                  />
+                ),
+              }
+            }}
+          />
+          <Stack.Screen
+            name="UserForm"
+            component={UserForm}
+            options={{
+              title: 'Formulário de Usuários',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  )
+}
+
+// const screenOptions = {
+//   headerShown: false,
+// }
